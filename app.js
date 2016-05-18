05cd0566ca6a8b8e372cc27255ab808431213b98
rajkumar <rajkumar.gootam@gmail.com>
2016-05-18 17:30:41 +0530

    We Created cricketdetails.html & We displayed the data using unique id we diplayed the data

--- app.js
+++ app.js
@@ -5,14 +5,35 @@ angularApp.config(function ($routeProvider){
   .when ('/',
   {
     templateUrl: 'pages/cricket.html',
-    controller: 'CricketController'
+    controller: 'CricketController',
+    controllerAs: 'hm'
+  })
+
+  .when ('/cricketdetails/:uniqId',
+  {
+    templateUrl: 'pages/cricketdetails.html',
+    controller: 'CricketDetailsController',
+    controllerAs:'dc'
   })
 });
 
 
-angularApp.controller("CricketController",function($resource){
+
+angularApp.controller("CricketController",['$resource','$filter',function($resource,$filter){
   var vm=this;
    var cricketResource = $resource('http://cricapi.com/api/cricket');
    vm.cricketResponse = cricketResource.get();
    console.log(vm.cricketResponse);
-});
+}]);
+
+
+
+angularApp.controller("CricketDetailsController",['$resource','$filter','$routeParams',
+function($resource,$filter,$routeParams){
+   var vm=this;
+   var id = $routeParams.uniqId;
+   var cricDetails = $resource('http://cricapi.com/api/cricketScore',{unique_id:id});
+   vm.cricResponse = cricDetails.get();
+   console.log(vm.cricResponse);
+
+}]);
